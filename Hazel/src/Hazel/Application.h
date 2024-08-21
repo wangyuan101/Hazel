b4ce6d6e12f3226f45aa036977b2064cf180258d
wangyuan <827411804@qq.com>
2024-08-21 12:12:04 +0800

    Added Window class and GLFW.

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Core.h"
 #include "Events/Event.h"
+#include "Window.h"
 
 namespace Hazel {
 	class HAZEL_API Application
@@ -10,6 +11,10 @@ namespace Hazel {
 		virtual ~Application();
 
 		void Run();
+
+	private:
+		std::unique_ptr<Window> m_Window;
+		bool m_Running = true;
 	};
 
 	// To be defined in CLIENT
--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Hazel/Core.h"
+#include "hzpch.h"
 
 namespace Hazel {
 
